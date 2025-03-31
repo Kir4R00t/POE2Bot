bdc476b4f83dae4343ee639029db5847a8f70a4d
Kir4R00t <miki4sa@interia.pl>
2025-03-31 21:13:16 +0200

    putting item names and their corresponding emojis in other file

---
+++ item_emojis.py
@@ -0,0 +1,65 @@
+#
+# Here is the list of all item names from poe2 api with their 
+#   corresponding discord emojis.
+#
+# (List got so big I decided to put it here to make it more readable)
+#
+
+list = {
+    # Currency
+    'alch': '<:alchemy:1355494399416471764>',
+    'annul': '<:annulment:1355494427501662311>',
+    'etcher': '<:arcanists_etcher:1356180485071441940>',
+    'artificers': '<:artificers_orb:1356180614209863730>',
+    'artificers-shard': '<:artificers_shard:1356180664013291643>',
+    'aug': '<:augment:1355494457923080222>',
+    'chance': '<:chance:1355493353004863488>',
+    'chaos': '<:chaos:1355492533236535366>',
+    'divine': '<:div:1355492390353502388>',
+    'exalted': '<:exalt:1355493424207237210>',
+    'gcp': '<:gemcutters_prism:1356180779012587632>',
+    'bauble': '<:glassblowers_bauble:1356180826903285825>',
+    "Greater Jeweller's Orb": '<:greater_jewellers_orb:1356180894485844009>',
+    "Lesser Jeweller's Orb": '<:lesser_jewellers_orb:1356180955731329115>',
+    'mirror': '<:mirrorr:1355493390401147023>',
+    "Perfect Jeweller's Orb": '<:perfect_jewellers_orb:1356181022227697694>',
+    'regal': '<:regal:1355494330571165736>',
+    'scrap': '<:scrap:1356180559076003850>',
+    'wisdom': '<:scroll_of_wisdom:1356181080993960088>',
+    'vaal': '<:vaal:1355492657375215646>',
+    'whetstone': '<:whetstone:1356180728844652567>',
+    'regal-shard': '<:regal_shard:1356184910464815225>',
+    'transmutation-shard': '<:transmutation_shard:1356185270705455195>',
+    'transmute': '<:transmutation:1355494366713745468>',
+    
+    # Soul
+    'soul-core-of-atmohua':'<:Soul_Core_of_Atmohua:1356330777222189187>',
+    'soul-core-of-azcapa':'<:Soul_Core_of_Azcapa:1356330778845516006>',
+    'soul-core-of-cholotl':'<:Soul_Core_of_Cholotl:1356330780107739366>',
+    'soul-core-of-citaqualotl':'<:Soul_Core_of_Citaqualotl:1356330752358355251>',
+    'soul-core-of-jiquani':'<:Soul_Core_of_Jiquani:1356330754283667670>',
+    'soul-core-of-opiloti':'<:Soul_Core_of_Opiloti:1356330756057862375>',
+    'soul-core-of-puhuarte':'<:Soul_Core_of_Puhuarte:1356330726651461672>',
+    'soul-core-of-quipolatl':'<:Soul_Core_ofQuipolatl:1356330728195096696>',
+    'soul-core-of-tacati':'<:Soul_Core_of_Tacati:1356330730069954791>',
+    'soul-core-of-ticaba':'<:Soul_Core_of_Ticaba:1356330731990679706>',
+    'soul-core-of-topotante':'<:Soul_Core_of_Topotante:1356330689863094356>',
+    'soul-core-of-tzamoto':'<:Soul_Core_of_Tzamoto:1356330691524034791>',
+    'soul-core-of-xopec':'<:Soul_Core_of_Xopec:1356330692673536152>',
+    'soul-core-of-zalatl':'<:Soul_Core_of_Zalatl:1356330693650550920>',
+    'soul-core-of-zantipi':'<:Soul_Core_of_Zantipi:1356330662310838423>',
+
+    # Breach catalysts
+    'skittering-catalyst': '<:Skittering_Catalyst:1356334422189670470>',
+    'sibilant-catalyst': '<:Sibilant_Catalyst:1356334420826787901>',
+    'reaver-catalyst': '<:Reaver_Catalyst:1356334419182358719>',
+    'neural-catalyst': '<:Neural_Catalyst:1356334497939067147>',
+    'flesh-catalyst': '<:Flesh_Catalyst:1356334496978305194>',
+    'eshs-catalyst': '<:Eshs_Catalyst:1356334495661428889>',
+    'chayulas-catalyst': '<:Chayulas_Catalyst:1356334494344544366>',
+    'carapace-catalyst': '<:Carapace_Catalyst:1356334492805107933>',
+    'adaptive-catalyst': '<:Adaptive_Catalyst:1356334491269988503>',
+    'xophs-catalyst': '<:Xophs_Catalyst:1356334426149093386>',
+    'uul-netols-catalyst': '<:UulNetols_Catalyst:1356334424941396069>',
+    'tuls-catalyst': '<:Tuls_Catalyst:1356334423100100832>'
+}
